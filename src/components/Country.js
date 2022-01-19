ab312195a58632c709123e4b756a79d9185a2208
Abu-Ubada2020 <abcoding17@gmail.com>
2022-01-19 09:33:11 +0530

    Resolved duplicate id and lazy loading image at start error in the HTML Validations report of the Challenge from Front End Mentor

--- src/components/Country.js
+++ src/components/Country.js
@@ -4,19 +4,20 @@ const Country = ({
 	country: { name, population, region, capital, flag, original_name },
 }) => {
 	var nameURIEndPoint = encodeURIComponent(original_name.toLowerCase());
-
-	var singleCountry =
+	var countryName = original_name.toLowerCase().split(" ")[0];
+	console.log("countryName", countryName);
+	var singleCountry = (
 		// prettier-ignore
-		<Link to={"country/" + nameURIEndPoint} className="single-country" aria-labelledby="country-name" aria-describedby="country-meta">
+		<Link to={"country/" + nameURIEndPoint} className="single-country" aria-labelledby={`country-name-${countryName}`} aria-describedby={`country-meta-${countryName}`}>
 			{/* <Link to="/invoices" className="single"> Abd </Link> */}
-			<img src={flag} alt={"Flag of " + name} className="country-flag" loading="lazy" onLoad={console.log(`${name} flag loaded`)}/>
+			<img src={flag} alt={"Flag of " + original_name} className="country-flag"/>
 			<div className="country-details">
 				<h2
 					className="country-name"
-					id="country-name"
+					id={`country-name-${countryName}`}
 					dangerouslySetInnerHTML={{ __html: name }}
 				></h2>
-				<div id="country-meta">
+				<div id={`country-meta-${countryName}`}>
 					<p className="country-population">
 						<b> Population: </b> {population.toLocaleString("en-IN")}
 					</p>
@@ -28,7 +29,7 @@ const Country = ({
 					</p>
 				</div>
 			</div>
-		</Link>;
+		</Link>);
 
 	return singleCountry;
 };
